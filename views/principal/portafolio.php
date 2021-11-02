155cee080a8a1d5004afada8f02de48c359504da
FSIce1 <luisfelipesiesquen22@gmail.com>
2021-11-02 12:34:32 -0500

    orden de los proyectos

--- views/principal/portafolio.php
+++ views/principal/portafolio.php
@@ -62,42 +62,6 @@
 
             <div class="proyectos">
 
-                <a href="https://fsice1.github.io/Mis-Cursos/" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Platzi-cursos.png" alt="Platzi Cursos">
-                    <p>Platzi Cursos</p>
-                    <div></div>
-                </a>
-
-                <a href="https://fsice1.github.io/Web-Homer/" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Web-Homer.png" alt="Web Homer">
-                    <p>Web Homer</p>
-                    <div></div>
-                </a>
-
-                <a href="https://fsice1.github.io/Taller-Practico-1-Css/" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Reto1-Css.png" alt="Taller Práctico Css">
-                    <p>Taller Práctico Css</p>
-                    <div></div>
-                </a>
-
-                <a href="https://codepen.io/LuisFelipe1/pen/QWgBMNj" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Mario-Bros.PNG" alt="Mario Bros">
-                    <p>Marios Bross</p>
-                    <div></div>
-                </a>
-
-                <a href="https://fsice1.github.io/Taller-Practico-Js/" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Taller-Práctico-Js.PNG" alt="Taller Práctico Js">
-                    <p>Taller Práctico Js</p>
-                    <div></div>
-                </a>
-
-                <a href="https://github.com/FSIce1/Venta-Bebidas-Alcoholicas" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Venta-Bebidas-Alcoholicas.png" alt="Venta de bebidas alchólicas">
-                    <p>Venta Bebidas Alcohólicas</p>
-                    <div></div>
-                </a>
-
                 <a href="https://github.com/FSIce1/Sistema-Incidencias" target="_blank" class="proyecto">
                     <img src="../../img/repositorios/Sistemas-Incidencias.png" alt="Sistemas de incidencias">
                     <p>Sistema De Incidencias</p>
@@ -110,21 +74,33 @@
                     <div></div>
                 </a>
 
-                <a href="https://fsice1.github.io/Taller-Practico-2-Css/index.html" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Taller-Práctico-2-Css.PNG" alt="Taller Práctico 2 Css">
-                    <p>Taller Práctico 2 - Css</p>
+                <a href="https://github.com/FSIce1/Venta-Bebidas-Alcoholicas" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Venta-Bebidas-Alcoholicas.png" alt="Venta de bebidas alchólicas">
+                    <p>Sistema De Venta De Bebidas</p>
+                    <div></div>
+                </a>
+
+                <a href="https://fsice1.github.io/Personajes-Rick-And-Morty/index" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Lista-Personajes.PNG" alt="Lista Rick and Morty">
+                    <p>Listado Personajes Rick and Morty</p>
                     <div></div>
                 </a>
 
                 <a href="https://fsice1.github.io/Taller-Practico-3-Css/public/header.html" target="_blank" class="proyecto">
                     <img src="../../img/repositorios/Taller-Práctico-3-Css.PNG" alt="Taller Práctico 3 Css">
-                    <p>Taller Práctico 3 - Css</p>
+                    <p>Carta De Un Restaurante</p>
                     <div></div>
                 </a>
 
                 <a href="https://fsice1.github.io/Taller-Practico-4-Css/index.html" target="_blank" class="proyecto">
                     <img src="../../img/repositorios/Taller-Práctico-4-Css.PNG" alt="Taller Práctico 4 Css">
-                    <p>Taller Práctico 4 - Css</p>
+                    <p>Web De Una Pollería</p>
+                    <div></div>
+                </a>
+
+                <a href="https://fsice1.github.io/Mis-Cursos/" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Platzi-cursos.png" alt="Platzi Cursos">
+                    <p>Platzi Cursos</p>
                     <div></div>
                 </a>
 
@@ -134,15 +110,15 @@
                     <div></div>
                 </a>
 
-                <a href="http://lista-cursos-tecnologia.herokuapp.com/" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Lista-Cursos-Aleatorios.PNG" alt="Lista Cursos Aleatorios">
-                    <p>Lista Cursos Aleatorios</p>
+                <a href="https://fsice1.github.io/Taller-Practico-2-Css/index.html" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Taller-Práctico-2-Css.PNG" alt="Taller Práctico 2 Css">
+                    <p>Web De DBZ</p>
                     <div></div>
                 </a>
 
-                <a href="https://fsice1.github.io/Taller-Practico-2-Js" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Taller-Practico-2-Js.PNG" alt="Taller Práctico 2 Js">
-                    <p>Taller Práctico 2 - Js</p>
+                <a href="http://lista-cursos-tecnologia.herokuapp.com/" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Lista-Cursos-Aleatorios.PNG" alt="Lista Cursos Aleatorios">
+                    <p>Lista Cursos Aleatorios</p>
                     <div></div>
                 </a>
 
@@ -152,9 +128,33 @@
                     <div></div>
                 </a>
 
-                <a href="https://fsice1.github.io/Personajes-Rick-And-Morty/index" target="_blank" class="proyecto">
-                    <img src="../../img/repositorios/Lista-Personajes.PNG" alt="Lista Rick and Morty">
-                    <p>Listado Personajes Rick and Morty</p>
+                <a href="https://fsice1.github.io/Web-Homer/" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Web-Homer.png" alt="Web Homer">
+                    <p>Web Homer</p>
+                    <div></div>
+                </a>
+
+                <a href="https://fsice1.github.io/Taller-Practico-Js/" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Taller-Práctico-Js.PNG" alt="Taller Práctico Js">
+                    <p>Descuentos y Figuras</p>
+                    <div></div>
+                </a>
+
+                <a href="https://fsice1.github.io/Taller-Practico-2-Js" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Taller-Practico-2-Js.PNG" alt="Taller Práctico 2 Js">
+                    <p>Reproductor Video</p>
+                    <div></div>
+                </a>
+
+                <a href="https://fsice1.github.io/Taller-Practico-1-Css/" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Reto1-Css.png" alt="Taller Práctico Css">
+                    <p>Taller Práctico Css</p>
+                    <div></div>
+                </a>
+
+                <a href="https://codepen.io/LuisFelipe1/pen/QWgBMNj" target="_blank" class="proyecto">
+                    <img src="../../img/repositorios/Mario-Bros.PNG" alt="Mario Bros">
+                    <p>Marios Bross</p>
                     <div></div>
                 </a>
 
